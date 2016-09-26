0698a7b7a1c62b940b0b9f67b05a4808c28674ad
antrivera <anthony.rivs@gmail.com>
2016-09-25 17:09:41 -0700

    implemented rounds

--- timer.jsx
+++ timer.jsx
@@ -6,9 +6,9 @@ class Timer extends React.Component {
 
     this.state = {
       workInterval: true,
-      round: 1,
+      completedRounds: 0,
       targetRounds: 4,
-      intervalLength: 60,
+      intervalLength: 10,
       elapsedTime: 0,
       timerActive: false
     };
@@ -43,19 +43,41 @@ class Timer extends React.Component {
   startTimer() {
     let initialOffset = 1257;
     this.setState({timerActive: true});
+
     this.timer = setInterval(() => {
       this.setState({elapsedTime: this.state.elapsedTime + 1});
+      let circle = document.getElementsByClassName('circle-animation');
+      let c = circle[0].style;
+      let updatedTime = this.state.elapsedTime + 1;
 
-      if (this.state.elapsedTime === this.state.intervalLength) {
-        clearInterval(this.timer);
-        document.body.style.background = '#5CBC9E';
+      if (this.state.elapsedTime % this.state.intervalLength === 0) {
+        if (this.state.workInterval) {
+          this.setState({
+            workInterval: false,
+            completedRounds: this.state.completedRounds + 1,
+            intervalLength: 5,
+            elapsedTime: 0
+          });
+          document.body.style.background = '#5CBC9E';
+          c.strokeDashoffset = initialOffset;
+        } else {
+          this.setState({
+            workInterval: true,
+            intervalLength: 10,
+            elapsedTime: 0
+          });
+          document.body.style.background = '#EC5E54';
+          c.strokeDashoffset = initialOffset;
+
+          if (this.state.completedRounds === this.state.targetRounds) {
+            clearInterval(this.timer);
+          }
+        }
+
         return;
       }
 
-      let circle = document.getElementsByClassName('circle-animation');
-      let c = circle[0].style;
-      c.strokeDashoffset = initialOffset-((this.state.elapsedTime+1)*((initialOffset)/this.state.intervalLength));
-
+      c.strokeDashoffset = initialOffset-((updatedTime)*((initialOffset)/this.state.intervalLength));
     }, 1000);
   }
 
@@ -71,7 +93,7 @@ class Timer extends React.Component {
         <svg width="500" height="500" viewBox="0 0 500 500">
           <g>
             <text id="timer-text" x="51%" y="-18%" textAnchor="middle" dominantBaseline="central" dy=".3em">
-              { this.timerDisplay(this.state.intervalLength - this.state.elapsedTime) }
+              { this.timerDisplay(Math.abs(this.state.intervalLength - this.state.elapsedTime)) }
             </text>
             <circle id="circle" className="circle-animation" r="200" cy="250" cx="250" strokeWidth="3" stroke="white" fill="transparent">
             </circle>
@@ -80,7 +102,7 @@ class Timer extends React.Component {
         { this.state.timerActive ? <div className="pause-btn" onClick={this.pauseTimer}></div> :
             <div className="play-btn" onClick={this.startTimer}></div>
         }
-        { "Round 1/4" }
+        <div>Round {this.state.completedRounds}/{this.state.targetRounds}</div>
       </div>
     );
   }
